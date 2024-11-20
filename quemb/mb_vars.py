4befd55cea4f00beec5ba713b04104d2801b5b07
Leah Weisburn <lweisbur@mit.edu>
2024-11-20 18:33:57 -0500

    Edit mb_vars

--- quemb/mb_vars.py
+++ quemb/mb_vars.py
@@ -1,8 +1,8 @@
 # Set relevant variables for the mixed-basis embedding code
 
 # Set the QuEmb be_var scratch levels
-scratch='/scratch/lweisbur/'
-create_scratch_directory=False
+scratch=''
+create_scratch_directory=True
 
 # Verbose levels: Quiet (0), Error (1), Warn (2), Note (3), Info (4), Debug (5)
 log_verbose_level=0
