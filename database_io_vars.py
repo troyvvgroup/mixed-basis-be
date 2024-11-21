8bad7ca8e1595cbc193279c4649851956b7e683b
Minsik Cho <mscho527@gmail.com>
2024-11-21 10:36:57 -0500

    Refactor database_io to comply with ruff checks and runtime vars separation

---
+++ database_io_vars.py
@@ -0,0 +1,7 @@
+"""
+This file contains the variables that are used in the database_io.py file.
+"""
+
+# Path to `pipeline` code
+# https://github.com/Quantum-for-Life/pipeline
+path_to_pipeline = "/home/minsik/pipeline/scripts/get-capped-qm-region"
