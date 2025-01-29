c071c65ff29552893df2e4019dfe5eaee0eecb63
Leah Weisburn <lweisbur@mit.edu>
2025-01-29 11:13:08 -0500

    Update mixed-basis-be to work with QuEmb

--- quemb/mb_vars.py
+++ quemb/mb_vars.py
@@ -1,9 +1,11 @@
 # Set relevant variables for the mixed-basis embedding code
 
-# Set the QuEmb be_var scratch levels
-scratch=''
-create_scratch_directory=True
+# Set the QuEmb scratch path
+# This will be used as the WorkDir.from_environment(user_defined_root=scratch_path)
+# This sets the root of the scratch, while WorkDir will generate the directory
+# name from Slurm or the PID
+# Else: jobs will be run in the default, /tmp, and ERIs deleted at end automatically
+scratch_path=''
 
 # Verbose levels: Quiet (0), Error (1), Warn (2), Note (3), Info (4), Debug (5)
 log_verbose_level=0
-
